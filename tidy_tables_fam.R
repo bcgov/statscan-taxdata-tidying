d0ad0e3530a45178f7b35b2e58675bf141015b74
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2019-03-20 21:31:25 -0700

    f-07 colnames working; add to fn

--- tidy_tables_fam.R
+++ tidy_tables_fam.R
@@ -20,11 +20,6 @@ library(stringr)
 library(tibble)
 
 
-filename <- "2016_Family_Tables_1_to_18_Canada.xls"
-filefolder <- "data-raw"
-filepath <- here(filefolder, filename)
-
-#--------------------------------------------------------------------
 ## Function to read in each sheet from Table F xls file, clean column names,
 ## and export df in a list object
 
@@ -36,6 +31,30 @@ tidy_tax_fam <- function(sheet, skip, col_names, path) {
     tools::file_path_sans_ext()
   
   file_year <- pathbase %>% stringr::str_extract("^[0-9]{4}")
+  
+  if(sheet == "7") {
+    
+    sheetcolnames <- read_xls(filepath, sheet = sheet, skip = 1, col_names = FALSE, n_max = 3) %>%
+      t() %>% 
+      as_tibble(.name_repair = ~ c("one", "two", "three"))
+    
+    sheetcolnames$three[141] <- "$'000"
+    sheetcolnames$three[144] <- "$'000"
+    sheetcolnames$three[142] <- "EDR"
+    sheetcolnames$three[145] <- "$EDR"
+    
+    sheetcolnames <- sheetcolnames %>% 
+      fill(one, two) %>%  # fill empty cells
+      unite(sheet_col_names) %>% 
+      mutate(sheet_col_names = tolower(str_replace_all(sheet_col_names, "\\s", "|")),
+             sheet_col_names = paste("F", sheet = sheet, sheet_col_names, sep = '|'),
+             sheet_col_names = str_replace(sheet_col_names, "_na_na|_na", "")) %>% 
+      select(sheet_col_names) %>% 
+      pull()
+    
+  }
+  
+  else {
   
   if (sheet %in% c("2", "3A", "3B", "3C", "13", "14A", "14B")) {
     tempcols <- c("one", "two")
@@ -53,25 +72,28 @@ tidy_tax_fam <- function(sheet, skip, col_names, path) {
     select(sheet_col_names) %>% 
     pull()
   
+  }
+  
   tidy_df <- path %>%
     read_excel(sheet = sheet, skip = 4,
                col_names = sheetcolnames,
                .name_repair = "unique") %>%
     tibble::add_column(year = file_year, .before = 1) 
   
-  # write_csv(tidy_df, paste0("data-tidy/", pathbase, "-", sheet, ".csv"))
+  write_csv(tidy_df, paste0("data-tidy/", file_year, "-FAM-", sheet, ".csv"))
   
   return(tidy_df)
 }
 
 
+#-------------------------------------------------------------------------------
 
-## example Table I .xls file
+## example Table F .xls file
 filename <- "2016_Family_Tables_1_to_18_Canada.xls"
-filefolder <- "data-raw/"
+filefolder <- "data-raw/fam"
 filepath <- here(filefolder, filename)
 
-## get sheet names from example Table I .xls file
+## get sheet names from example Table F .xls file
 sheets <- excel_sheets(filepath)
 
 ## read one sheet by sheet name 
@@ -84,63 +106,59 @@ compare <- F_test %>% select(contains(".."))
 
 
 ## Read in and add names to all sheets at once
-tidy_sheets <- filepath %>%
+tidy_fam_sheets <- filepath %>%
   excel_sheets() %>%
   set_names() %>% 
   map(tidy_tax_fam, path = filepath)
 
 
-## Save all sheets as individual CSVs
-tidy_sheets %>%
-  names(.) %>%
-  walk(~ write_csv(tidy_sheets[[.]], paste0("data-tidy/", "2016_Family_BC", "-", ., ".csv")))
+# ## Save all sheets as individual CSVs
+# tidy_sheets %>%
+#   names(.) %>%
+#   walk(~ write_csv(tidy_sheets[[.]], paste0("data-tidy/", "2016", "-FAM-", ., ".csv")))
+
 
 #--------------------------------------------------------------------
-
-## Fix Table F-07 xls file
-
-
+## Fix sheet 7  column names
 filename <- "2016_Family_Tables_1_to_18_Canada.xls"
-filefolder <- "data-raw"
+filefolder <- "data-raw/fam"
 filepath <- here(filefolder, filename)
 
 sheet_7_colnames <- read_xls(filepath, sheet = "7", skip = 1, col_names = FALSE, n_max = 3) %>%
   t() %>% 
-  as_tibble(.name_repair = ~ c("one", "two", "three")) %>% 
-  mutate(four = c("NA", (1:nrow))) %>% ##NOT
-  select(one, two, three, four) %>% ##NOT
-  fill(one, two, three, four) %>%  # fill empty cells
-  unite(sheet_7_colnames) %>% 
-  mutate(sheet_7_colnames = tolower(str_replace_all(sheet_7_colnames, "\\s", "|")),
-         sheet_7_colnames = paste("F",sheet = "7", sheet_7_colnames, sep = '|'),
-         sheet_7_colnames = str_replace(sheet_7_colnames, "_na_na|_na", "")) %>% 
-  select(sheet_7_colnames) %>% 
+  as_tibble(.name_repair = ~ c("one", "two", "three"))
+
+sheet_7_colnames$three[141] <- "$'000"
+sheet_7_colnames$three[144] <- "$'000"
+sheet_7_colnames$three[142] <- "EDR"
+sheet_7_colnames$three[145] <- "$EDR"
+  
+sheet_7_colnames <- sheet_7_colnames %>% 
+  fill(one, two) %>%  # fill empty cells
+  unite(sheet_col_names) %>% 
+  mutate(sheet_col_names = tolower(str_replace_all(sheet_col_names, "\\s", "|")),
+         sheet_col_names = paste("F", sheet = "7", sheet_col_names, sep = '|'),
+         sheet_col_names = str_replace(sheet_col_names, "_na_na|_na", "")) %>% 
+  select(sheet_col_names) %>% 
   pull()
-
-
-
-sheet_7_colnames
-
-my_custom_header_repair <- str_replace_all(as.character(my_custom_name_repair), c(" |/|'|"), "")
 
 
 # read in sample xlsx data and use new column names
 
-sheet_data <- read_xls(filepath, sheet = 12,
-                       col_names = my_custom_header_repair)
-colnames(sheet_data)
-
-#OR
-colClean <- function(x){
-  colnames(x) <- gsub(".", "", perl=TRUE, colnames(x))
-}
-colClean(sheet_data)
-colnames(sheet_data)
+sheet_7_data <- read_xls(filepath, sheet = "7",
+                       col_names = sheet_7_colnames)
+colnames(sheet_7_data)
 
 
+#--------------------------------------------------------------------
 
 
+###########
+## TO DO ##
+###########
 
-
+## Add in the filtering of BC rows into the function
+## Add loop to read all files
+## Add loop to read and bind csv by table
 
 
