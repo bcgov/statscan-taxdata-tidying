f20ed8c7c91ece0409c74d36ae6e34186e6c6f08
stephhazlitt <stephanie.hazlitt@gov.bc.ca>
2019-03-25 12:00:42 -0700

    add setup script

---
+++ setup.R
@@ -0,0 +1,53 @@
+# Copyright 2019 Province of British Columbia
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and limitations under the License.
+
+
+# Install & Load packages
+pkgs <-c("plyr", "janitor", "tibble", "stringr", "tidyr", "here", "readr", "purrr", "readxl", "dplyr")
+check <- sapply(pkgs,require,warn.conflicts = TRUE,character.only = TRUE)
+if(any(!check)) {
+  pkgs.missing <- pkgs[!check]
+  install.packages(pkgs.missing)
+  check <- sapply(pkgs.missing, require, warn.conflicts = TRUE, character.only = TRUE)
+}
+
+
+## Function to get year from file name 
+get_file_year <- function(path) {
+  # find file_year
+  pathbase <- path %>%
+    basename() %>%
+    tools::file_path_sans_ext()
+  
+  # assign file_year
+  file_year <- pathbase %>% stringr::str_extract("^[0-9]{4}")
+  return(file_year)
+}
+
+
+## Function to clean column sheet names  (for most sheets)
+mutate_col_names <- function(sheet_col_names) {
+  sheet_col_names = str_trim(sheet_col_names)
+  sheet_col_names = tolower(str_replace_all(sheet_col_names, "\\s", "|"))
+  sheet_col_names = str_replace_all(sheet_col_names, "_", "|")
+  sheet_col_names = str_replace_all(sheet_col_names, "na|na||", "")
+  sheet_col_names = str_replace_all(sheet_col_names, "\\|\\|", "|")
+  sheet_col_names = str_replace_all(sheet_col_names, "change\\|\\d{4}-\\d{4}", "range|last|5years")
+  return(sheet_col_names)
+}
+
+########
+## Add more functions common across all 3 scripts....
+########
+
+## Invisible header object
+.setup_sourced <- TRUE
